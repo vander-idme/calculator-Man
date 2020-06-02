b4ad3a3cb02b06fe513fee184ba62c714c0b502a
vander-idme <idmevander@gmail.com>
2020-06-01 20:42:49 -0500

    Reparando Bugs de dependencias

--- dist/main.js
+++ dist/main.js
@@ -86,14 +86,27 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/Calculator.js":
+/*!***************************!*\
+  !*** ./src/Calculator.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nfunction Calculator(config) {\n  this.pantalla = config.pantalla;\n  this.btns = config.buttons; // console.log(this.btns)\n\n  this._listerner();\n}\n\nCalculator.prototype._listerner = function () {\n  this.btns.forEach(button => {\n    this.clickButtons(button);\n  });\n};\n\nCalculator.prototype.print = function (num) {\n  this.pantalla.value = this.pantalla.value + num;\n};\n\nCalculator.prototype.clear = function () {\n  this.pantalla.value = null;\n};\n\nCalculator.prototype.equal = function () {\n  let exp = this.pantalla.value;\n\n  if (exp) {\n    this.pantalla.value = eval(exp);\n  }\n};\n\nCalculator.prototype.backSpace = function () {\n  let exp = this.pantalla.value;\n  let erase = \"\"; // let caracter = erase.charAt(erase.length - 1)\n\n  for (let i = 0; i < exp.length - 1; i++) {\n    erase = erase + exp[i];\n  }\n\n  this.pantalla.value = erase;\n};\n\nCalculator.prototype.clickButtons = function (button) {\n  // console.log(button.value)\n  if (button.value >= 0 && button.value <= 9) {\n    button.onclick = () => {\n      this.print(button.value);\n    };\n  }\n\n  if (button.value === 'C') {\n    button.onclick = () => {\n      this.clear();\n    };\n  }\n\n  if (button.value === '+') {\n    button.onclick = () => {\n      this.print(button.value);\n    };\n  }\n\n  if (button.value === '-') {\n    button.onclick = () => {\n      this.print(button.value);\n    };\n  }\n\n  if (button.value === '*') {\n    button.onclick = () => {\n      this.print(button.value);\n    };\n  }\n\n  if (button.value === '/') {\n    button.onclick = () => {\n      this.print(button.value);\n    };\n  }\n\n  if (button.value === '=') {\n    button.onclick = () => {\n      this.equal();\n    };\n  }\n\n  if (button.value === '<') {\n    button.onclick = () => {\n      this.backSpace();\n    };\n  }\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Calculator);\n\n//# sourceURL=webpack:///./src/Calculator.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("function insert(num) {\n  document.form.textview.value = document.form.textview.value + num;\n}\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Calculator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Calculator.js */ \"./src/Calculator.js\");\n\nconst pantalla = document.querySelector(\"#pantalla\");\nconst buttonZero = document.querySelector(\"#numberZero\");\nconst buttonOne = document.querySelector(\"#numberOne\");\nconst buttonTwo = document.querySelector(\"#numberTwo\");\nconst buttonThree = document.querySelector(\"#numberThree\");\nconst buttonFour = document.querySelector(\"#numberFour\");\nconst buttonFive = document.querySelector(\"#numberFive\");\nconst buttonSix = document.querySelector(\"#numberSix\");\nconst buttonSeven = document.querySelector(\"#numberSeven\");\nconst buttonEight = document.querySelector(\"#numberEight\");\nconst buttonNine = document.querySelector(\"#numberNine\");\nconst minus = document.querySelector(\"#minus\");\nconst plus = document.querySelector(\"#plus\");\nconst division = document.querySelector(\"#division\");\nconst multiplication = document.querySelector(\"#multiplication\");\nconst equal = document.querySelector(\"#equal\");\nconst backSpace = document.querySelector(\"#backSpace\");\nconst clear = document.querySelector(\"#clear\");\nconst calc = new _Calculator_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]({\n  pantalla: pantalla,\n  buttons: [buttonZero, buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix, buttonSeven, buttonEight, buttonNine, minus, plus, division, multiplication, equal, backSpace, clear]\n});\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
